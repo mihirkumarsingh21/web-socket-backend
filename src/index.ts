f1b7fdcf16262b48519ffbe0303534b2ba219f23
mihirkumarsingh21 <singhmihirkumar783@gmail.com>
2025-10-27 23:18:32 +0530

    imported socket io package

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,45 @@
-import express from "express";
+import express, { Application } from "express";
+import { Server, Socket } from "socket.io";
 import http from "http";
-import {WebSocketServer, WebSocket} from "ws";
 
-const app = express();
+
+
+interface ServerToClientEvents {
+  noArg: () => void;
+  basicEmit: (a: number, b: string, c: Buffer) => void;
+  withAck: (d: string, callback: (e: number) => void) => void;
+}
+
+interface ClientToServerEvents {
+  hello: () => void;
+}
+
+interface InterServerEvents {
+  ping: () => void;
+}
+
+
+const app: Application = express();
+
 const server = http.createServer(app);
 
-const wss = new WebSocketServer({server});
+const io = new Server < ServerToClientEvents, InterServerEvents, ClientToServerEvents > (server);
 
-// WEB SOCKET.
-
-wss.on("connection", (socket: WebSocket) => {
-    console.log(`socket connected successgully`);    
-
+io.on("connection", (socket: Socket) => {
+    console.log(`socket connected. ${socket.id}`);
     socket.on("message", (message) => {
-        console.log(`message: ${message}`);
-        socket.send("hello from server");
-        
+      
+      
     })
 })
 
 
-// http request.
+
 
 
 const port = 3000;
 
 server.listen(port, () => {
-    console.log(`server is running at port -> http://localhost:${port}`);
+    console.log(`server is running at port http://localhost:${port}`);
+    
 })
